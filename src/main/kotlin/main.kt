f29bd7615b5e5099690b9afcbec949572537946a
Archie <alimkul.shynggys@gmail.com>
2021-02-01 00:17:32 +0600

    Lesson 2.2

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var likes : Int =31
+    if(likes %10 ==1){
+        println("Понравилось "+ likes+ " челвеку")
+    }
+    else
+        println("Понравилось "+ likes+ " людям")
+
+}
